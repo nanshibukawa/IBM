a7c774c41b71e36481b443310ceb98585d0938ce
NanShibukawa <nanshibukawa@gmail.com>
2024-04-30 12:03:36 -0300

    teste final

--- python_teste.py
+++ python_teste.py
@@ -1,6 +1,2 @@
-print("Hello World")
-<<<<<<< HEAD
-print("testeanfa")
-=======
-print("Teste pull request)
->>>>>>> b33709e50b8302da4fd294a297e5eab6c02b6007
+print("Hello World!!!!")
+print("testeanfa!!!")
